9a2207dc3c09c5b1787ae627c5cb81a6598a5a10
anishidra76 <anishidradeveloper76@gmail.com>
2026-09-18 20:17:02 +0100

    Fix Sitemap

--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("home/", views.home, name="home"),
     path("register/", views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="notes/login.html"), name="login"),
