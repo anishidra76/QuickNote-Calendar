67b49019382c3bf45336e513beb76f4252d509b4
anishidra76 <anishidradeveloper76@gmail.com>
2026-09-18 20:02:32 +0100

    Fix Sitemap

--- notes/sitemaps.py
+++ notes/sitemaps.py
@@ -10,8 +10,6 @@ class StaticViewSitemap(Sitemap):
     def items(self):
         return [
             "home",
-            "register",
-            "login",
         ]
 
     def location(self, item):
